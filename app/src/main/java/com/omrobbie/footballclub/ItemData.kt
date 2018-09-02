ffb30878089853919e169845f3de8114df2e8674
omrobbie <putu.roby@yahoo.com>
2018-09-02 11:32:25 +0700

    activate experimental for android extensions

---
+++ app/src/main/java/com/omrobbie/footballclub/ItemData.kt
@@ -0,0 +1,7 @@
+package com.omrobbie.footballclub
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class ItemData(val image: Int?, val name: String?): Parcelable
