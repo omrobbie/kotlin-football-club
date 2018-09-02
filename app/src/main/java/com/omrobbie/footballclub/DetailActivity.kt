fde1e235140bb9a71230460264a5eb2ef872e90e
omrobbie <putu.roby@yahoo.com>
2018-09-02 22:45:22 +0700

    setup detail layout

--- app/src/main/java/com/omrobbie/footballclub/DetailActivity.kt
+++ app/src/main/java/com/omrobbie/footballclub/DetailActivity.kt
@@ -1,24 +1,57 @@
 package com.omrobbie.footballclub
 
-import android.support.v7.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
-import android.view.View
+import android.support.v7.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import org.jetbrains.anko.*
 
 class DetailActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         DetailActivityUI().setContentView(this)
     }
 
-    inner class DetailActivityUI: AnkoComponent<DetailActivity> {
+    inner class DetailActivityUI : AnkoComponent<DetailActivity> {
+        val id_view = 1
+        val id_image = 2
+        val id_name = 3
+
         override fun createView(ui: AnkoContext<DetailActivity>) = with(ui) {
-            verticalLayout {
-                padding = dip(16)
+            relativeLayout {
+                lparams(wrapContent, wrapContent)
+
+                view {
+                    id = id_view
+                    setBackgroundColor(Color.rgb(126, 203, 238))
+                }.lparams(matchParent, dip(200))
+
+                imageView {
+                    id = id_image
+                    Glide.with(this)
+                            .load(android.R.drawable.ic_delete)
+                            .into(this)
+                }.lparams(dip(100), dip(100)) {
+                    centerHorizontally()
+                    topMargin = dip(150)
+                }
 
                 textView {
-                    setText("This is detail activity")
+                    id = id_name
+                    text = "Football Club FC"
+                    textSize = 24f
+                    setTypeface(null, Typeface.BOLD)
+                }.lparams {
+                    below(id_image)
+                    centerHorizontally()
+                }
+
+                textView {
+                    padding = dip(16)
+                    text = "Description"
+                }.lparams {
+                    below(id_name)
                 }
             }
         }
