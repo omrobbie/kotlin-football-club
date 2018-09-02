4070a4326f7b16822220dfbc92e5726f4fdec4a5
omrobbie <putu.roby@yahoo.com>
2018-09-02 22:49:09 +0700

    activate back button

--- app/src/main/java/com/omrobbie/footballclub/DetailActivity.kt
+++ app/src/main/java/com/omrobbie/footballclub/DetailActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import android.graphics.Typeface
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import com.bumptech.glide.Glide
 import org.jetbrains.anko.*
 
@@ -11,6 +12,17 @@ class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         DetailActivityUI().setContentView(this)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return if (item?.itemId == android.R.id.home) {
+            finish()
+            true
+        } else {
+            super.onOptionsItemSelected(item)
+        }
     }
 
     inner class DetailActivityUI : AnkoComponent<DetailActivity> {
